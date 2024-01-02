7b264d6637d1f40a4924bae26ea92cfce4458d6a
gumquat <gumquat@gmail.com>
2024-01-02 14:05:40 -0600

    doing 2

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -1,5 +1,18 @@
 #!/usr/bin/env python3
+
+"""description text goes here"""
+
 from typing import floor, Union
+import math
 
 def floor (n: Union[int, float]) -> Union[int, float]:
+
+  """_summary_
+
+  Args:
+      n (Union[int, float]): _description_
+
+  Returns:
+      Union[int, float]: _description_
+  """
   return floor(n)
