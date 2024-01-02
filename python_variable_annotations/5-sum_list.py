c6fe3b14e32c66b7d15d727d9e7f5a0eb5428ab5
gumquat <gumquat@gmail.com>
2024-01-02 15:39:57 -0600

    omfg

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
-"""function sum_list that takes input_list of floats and returns sum"""
+"""Description text goes here"""
 from typing import List
 
 
 def sum_list(input_list: List[float]) -> float:
     """
-    Calculate the sum of a list of floats and return the result
+    Description text goes here
     """
     return sum(input_list)
