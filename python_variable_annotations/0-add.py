7c03867730104917749c85e3e6048e0c5dfe2a6c
gumquat <gumquat@gmail.com>
2024-01-02 14:02:20 -0600

    Ahhahahhahah

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 
 """add two given integers"""
-  
-  
+
 def add(a: float, b: float) -> float:
+
   """
   Parameters: a and b floats
   Returns: sum of a and b
--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -4,6 +4,7 @@
 
 
 def concat (str1: str, str2: str) -> str:
+
     """
     type-annotated func 'concat' returns concat
     """
