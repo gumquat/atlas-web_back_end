749d12511e8f61b39efe0350a4b2470e3adc678e
gumquat <gumquat@gmail.com>
2024-01-02 13:55:03 -0600

    1 fix

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -3,8 +3,8 @@
 """type-annotated func 'concat' returns concat"""
 
 
-"""
+def concat (str1: str, str2: str) -> str:
+  """
 type-annotated func 'concat' returns concat
 """
-def concat (str1: str, str2: str) -> str:
   return str1 + str2
