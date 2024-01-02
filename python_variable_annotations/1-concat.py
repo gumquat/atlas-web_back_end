2642f479761f1e0dcb5024a511fc572b37650368
Evan Newman-Chock <gumquat@gmail.com>
2024-01-02 15:17:46 -0600

    Update 1-concat.py

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -4,14 +4,14 @@
 
 
 def concat(str1: str, str2: str) -> str:
-	"""
-	_summary_
+    """
+    _summary_
 
-	Args:
-		str1 (str): _description_
-		str2 (str): _description_
+    Args:
+        str1 (str): _description_
+        str2 (str): _description_
 
-	Returns:
-		str: _description_
-	"""
-	return str1 + str2
+    Returns:
+        str: _description_
+    """
+    return str1 + str2
