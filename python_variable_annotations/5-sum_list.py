df26e9a951fb4d45956d2d16831b8489786a5a99
gumquat <gumquat@gmail.com>
2024-01-02 15:36:28 -0600

    ahhhhhhhhhhhhhhhh

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -7,10 +7,8 @@ from typing import List
 def sum_list(input_list: List[float]) -> float:
     """
     _summary_
-
     Args:
         input_list (List[float]): _description_
-
     Returns:
         float: _description_
     """
