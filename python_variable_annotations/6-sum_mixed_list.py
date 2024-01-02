4ff7848f2a58b26e3a15159ff83bbb0e6936cbfd
gumquat <gumquat@gmail.com>
2024-01-02 15:41:03 -0600

    description

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -1,17 +1,11 @@
 #!/usr/bin/env python3
 
 """Description text goes here"""
-from typing import List, Union, float
+from typing import List, Union
 
 
 def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """
-    _summary_
-
-    Args:
-    mxd_lst (List[Union[int, float]]): _description_
-
-    Returns:
-    float: _description_
+    Description text goes here
     """
     return sum(mxd_lst)
