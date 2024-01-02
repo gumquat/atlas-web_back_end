be18e6d5330ea3edf0473c2147e9e55e3a89a0b6
gumquat <gumquat@gmail.com>
2024-01-02 15:39:04 -0600

    5

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -6,6 +6,6 @@ from typing import List
 
 def sum_list(input_list: List[float]) -> float:
     """
-    Calculate the sum of a list of floats and return the result
+    Description text goes here
     """
     return sum(input_list)
