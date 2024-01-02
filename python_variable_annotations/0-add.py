c37467fc0148e10416cde5475979a07e9faefa87
gumquat <gumquat@gmail.com>
2024-01-02 13:53:18 -0600

    changes to 0 and 1

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
+
+"""add two given integers"""
+  
+  
 def add(a: float, b: float) -> float:
   """
   Parameters: a and b floats
--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 
-"""Description: Write a type-annotated function concat that takes a string str1 and a string str2 as arguments and returns a concatenated string """
+"""type-annotated func 'concat' returns concat"""
 
 
-from typing import str
-"""Description: Write a type-annotated function concat that takes a string str1 and a string str2 as arguments and returns a concatenated string """
+"""
+type-annotated func 'concat' returns concat
+"""
 def concat (str1: str, str2: str) -> str:
   return str1 + str2
