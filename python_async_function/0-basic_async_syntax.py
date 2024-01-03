7119ad141d73898ada252515e10cef5aa7842c5d
gumquat <gumquat@gmail.com>
2024-01-03 15:00:08 -0600

    im losing my mind

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -5,7 +5,7 @@ import asyncio
 import random
 
 
-async def wait_random (max_delay: int = 10) -> float:
+async def wait_random(max_delay: int = 10) -> float:
     """
     Args:
     max_delay (int, optional): Defaults to 10.
