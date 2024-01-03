c706560cc2530048b132eee82ac0bfb71cf4e2b9
gumquat <gumquat@gmail.com>
2024-01-03 14:45:33 -0600

    numbah 1

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -6,15 +6,15 @@ import random
 
 
 async def wait_random (max_delay: int = 10) -> float:
-	"""
-	_summary_
+    """
+    _summary_
 
-	Args:
-	max_delay (int, optional): _description_. Defaults to 10.
+    Args:
+    max_delay (int, optional): _description_. Defaults to 10.
 
-	Returns:
-	float: _description_
-	"""
-	delay = random.uniform(0, max_delay)
-	await asyncio.sleep(delay)
-	return delay
+    Returns:
+    float: _description_
+    """
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
