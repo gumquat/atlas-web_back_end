e8892d6cfa4b398279fa4126a9d4501f423dc967
gumquat <gumquat@gmail.com>
2024-01-03 14:26:08 -0600

    numbah 0

---
+++ python_async_function/basic_async_syntax.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+"""asynchronous functon that waits a random number of seconds"""
+import asyncio
+import random
+
+
+async def wait_random (max_delay: int = 10) -> float:
+	"""
+	_summary_
+
+	Args:
+		max_delay (int, optional): _description_. Defaults to 10.
+
+	Returns:
+		float: _description_
+	"""
+	delay = random.uniform(0, max_delay)
+	await asyncio.sleep(delay)
+	return delay
