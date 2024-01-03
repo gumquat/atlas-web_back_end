83a49a4544eb97d53e141375cef0c5c59a6f240b
gumquat <gumquat@gmail.com>
2024-01-03 14:28:31 -0600

    fixed indents

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -10,10 +10,10 @@ async def wait_random (max_delay: int = 10) -> float:
 	_summary_
 
 	Args:
-		max_delay (int, optional): _description_. Defaults to 10.
+	max_delay (int, optional): _description_. Defaults to 10.
 
 	Returns:
-		float: _description_
+	float: _description_
 	"""
 	delay = random.uniform(0, max_delay)
 	await asyncio.sleep(delay)
