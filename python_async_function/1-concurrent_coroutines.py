857f460dcb427febe334110579f9a8cceb5a3afe
gumquat <gumquat@gmail.com>
2024-01-04 13:16:06 -0600

    float for 1

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -5,7 +5,7 @@ from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int = 10) -> List:
+async def wait_n(n: int, max_delay: int = 10) -> List[float]:
     """
     Args:
         n (int): _description_
