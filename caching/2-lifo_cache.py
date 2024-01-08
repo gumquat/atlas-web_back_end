1929778063d0fbd22874f313685981a65763938a
gumquat <gumquat@gmail.com>
2024-01-08 14:59:14 -0600

    make sure to print DISCARD

--- caching/2-lifo_cache.py
+++ caching/2-lifo_cache.py
@@ -20,13 +20,11 @@ class LIFOCache(BaseCaching):
         add item to the cache
         """
         if key is not None and item is not None:
-            self.cache_data[key] = item
-            if key not in self.key_order:
-                self.key_order.append(key)
-            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-                last_key = self.key_order.pop(-1)
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                last_key = list(self.cache_data.keys())[-1]
                 del self.cache_data[last_key]
-                print(f"DISCARD: {last_key}")
+                print("DISCARD:", last_key)
+            self.cache_data[key] = item
 
     def get(self, key):
         """
