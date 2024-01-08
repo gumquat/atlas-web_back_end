cddf12013ac49ce5eb8fd28a58e25e7fab91f63d
gumquat <gumquat@gmail.com>
2024-01-08 12:49:45 -0600

    holy wow i overtyped and im sick with covid/flu or something

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -1,47 +1,24 @@
 #!/usr/bin/python3
 
 """A class BasicCache that inherits from BaseCaching and is a caching system"""
-
-
-class BaseCaching:
-    """ BaseCaching defines:
-    - constants of your caching system
-    - where your data are stored (in a dictionary)
-    """
-    MAX_ITEMS = 4
-
-    def __init__(self):
-        """ Initiliaze
-        """
-        self.cache_data = {}
-
-    def print_cache(self):
-        """ Print the cache
-        """
-        print("Current cache:")
-        for key in sorted(self.cache_data.keys()):
-            print("{}: {}".format(key, self.cache_data.get(key)))
-
-    def put(self, key, item):
-        """ Add an item in the cache
-        """
-        raise NotImplementedError("put must be implemented in your cache class")
-
-    def get(self, key):
-        """ Get an item by key
-        """
-        raise NotImplementedError("get must be implemented in your cache class")
+BaseCaching = __import__('base_caching').BaseCaching
 
 class BasicCache(BaseCaching):
     """
-    Args:
-        BaseCaching (_type_): _description_
+    We cashing basics :sunglasses:
     """
+
     def put(self, key, item):
+        """
+        adds an item to the cache
+        """
         if key is not None and item is not None:
             self.cache_data[key] = item
 
     def get(self, key):
+        """
+        retrieves an item from the cache
+        """
         if key is None or key not in self.cache_data:
             return None
         return self.cache_data[key]
