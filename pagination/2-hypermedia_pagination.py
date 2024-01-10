bd8adf2da3eaedca4a2dcb36ca4ca8d377050138
gumquat <gumquat@gmail.com>
2024-01-10 15:32:26 -0600

    indention

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -64,40 +64,40 @@ class Server:
                 else []
 
 
-def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
-    """
-    Args:
-        page (int, optional): Defaults to 1.
-        page_size (int, optional): Defaults to 10.
-    Returns:
-        dict: dictionary containing page details
-    """
-    assert isinstance(page, int) and page > 0, \
-        "Page must be an integer greater than 0."
-    assert isinstance(page_size, int) and page_size > 0,\
-        "Page size must be an integer greater than 0."
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
+        """
+        Args:
+            page (int, optional): Defaults to 1.
+            page_size (int, optional): Defaults to 10.
+        Returns:
+            dict: dictionary containing page details
+        """
+        assert isinstance(page, int) and page > 0, \
+            "Page must be an integer greater than 0."
+        assert isinstance(page_size, int) and page_size > 0,\
+            "Page size must be an integer greater than 0."
 
-    dataset = self.dataset()
-    total_records = len(dataset)
-    total_pages = math.ceil(total_records / page_size)
+        dataset = self.dataset()
+        total_records = len(dataset)
+        total_pages = math.ceil(total_records / page_size)
 
-    start_index, end_index = index_range(page, page_size)
+        start_index, end_index = index_range(page, page_size)
 
-    with open('Popular_Baby_Names.csv', 'r', newline='', encoding='utf-8')\
-            as csvfile:
-        reader = csv.reader(csvfile)
-        rows = list(reader)
+        with open('Popular_Baby_Names.csv', 'r', newline='', encoding='utf-8')\
+                as csvfile:
+            reader = csv.reader(csvfile)
+            rows = list(reader)
 
-        data = rows[start_index:end_index] if start_index < len(rows) \
-            else []
+            data = rows[start_index:end_index] if start_index < len(rows) \
+                else []
 
-    hyper_info = {
-        'page_size': len(data),
-        'page': page,
-        'data': data,
-        'next_page': page + 1 if page < total_pages else None,
-        'prev_page': page - 1 if page > 1 else None,
-        'total_pages': total_pages
-    }
+        hyper_info = {
+            'page_size': len(data),
+            'page': page,
+            'data': data,
+            'next_page': page + 1 if page < total_pages else None,
+            'prev_page': page - 1 if page > 1 else None,
+            'total_pages': total_pages
+        }
 
-    return hyper_info
+        return hyper_info
