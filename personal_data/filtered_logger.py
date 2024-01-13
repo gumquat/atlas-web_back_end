c9de9b70cd0ffcdb47bae09db3b7f5bf8a976342
gumquat <gumquat@gmail.com>
2024-01-13 15:40:59 -0600

    redo 0

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -3,9 +3,10 @@
 """filtered logger module that obfuscates data in log messages"""
 import logging
 import re
+from typing import List
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
     """_summary_
 
     Args:
@@ -19,4 +20,6 @@ def filter_datum(fields, redaction, message, separator):
     Returns:
         _type_: the log message of obfuscated data
     """
-    return re.sub(fr'(?<={separator}|^)({"|".join(fields)})={redaction}(?={separator}|$)', f'{fields[0]}={redaction}', message)
+    regex = '(' + '|'.join(fields) + r')=[^' + re.escape(separator) + r']*'
+    obfuscated_message = lambda match: match.group(1) + '=' + redaction
+    return re.sub(regex, obfuscated_message, message)
