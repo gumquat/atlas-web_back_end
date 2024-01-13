06601e726812523ce6872afaf3f56a8f796975d9
gumquat <gumquat@gmail.com>
2024-01-13 15:42:31 -0600

    pycodestyle fixes

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -6,16 +6,17 @@ import re
 from typing import List
 
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+def filter_datum(fields: List[str], redaction: str, message: str,
+                separator: str) -> str:
     """_summary_
 
     Args:
-        fields (_type_): list of strings representing all 
+        fields (_type_): list of strings representing all
             fields to obfuscate
-        redaction (_type_): string representing by what the 
+        redaction (_type_): string representing by what the
             field will be obfuscated
         message (_type_): string representing the log line
-            separator (_type_): string representing by which character 
+            separator (_type_): string representing by which character
             is separating all fields in the log line
     Returns:
         _type_: the log message of obfuscated data
