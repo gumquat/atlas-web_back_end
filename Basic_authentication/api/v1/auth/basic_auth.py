2fcf7e341ac54e0f7c1bee84de9f847a70fc8f28
gumquat <gumquat@gmail.com>
2024-01-17 16:47:54 -0600

    number 7 :sunglasses emoji:

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -15,4 +15,21 @@ class BasicAuth(Auth):
     Class BasicAuth
     Inherits from Auth.py
     """
-    pass
+    def extract_base64_authorization_header(
+        self, authorization_header: str) -> str:
+        """Extract the Base64 part from the Authorization header
+        Args:
+            authorization_header (str): The Authorization header
+        Returns:
+            str: The Base64 part or None if the header is not valid
+        """
+        if authorization_header is None or not isinstance(
+            authorization_header, str):
+            return None
+
+        if not authorization_header.startswith("Basic "):
+            return None
+
+        # extracts the Base64 part after "Basic "
+        base64_part = authorization_header[len("Basic "):].strip()
+        return base64_part
