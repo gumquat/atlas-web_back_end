9c355c639f08328211a8222d3f134ee96ffd4923
gumquat <gumquat@gmail.com>
2024-01-17 14:11:42 -0600

    wait one more documentation

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -1,35 +1,41 @@
 #!/usr/bin/env python3
 
-""" Create a class to manage an API authenication """
+"""documentation string here"""
 
+from typing import List
+from typing import TypeVar
 from flask import request
-from typing import List, TypeVar
 
 
 class Auth:
-    """ Auth class for API authentication """
-
+    """auth class for the API"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """Check if authentication is required for the given path"""
-        if path is None or excluded_paths is None or not excluded_paths:
-            return True
+        """Check if authentication is required for the given path.
+        Args:
+        - path (str): The path to check for authentication requirement.
+        - excluded_paths (List[str]): List of paths to be excluded 
+        from authentication.
+        Returns:
+        - bool: True if authentication is required, False otherwise.
+        """
+        return False
 
-        normal_path = path if path.endswith("/") else path + "/"
-        for excluded_path in excluded_paths:
-            if excluded_path.endswith("/"):
-                if normal_path == excluded_path:
-                    return False
-            else:
-                if path.startswith(excluded_path):
-                    return False
-        return True
 
     def authorization_header(self, request=None) -> str:
-        """Get the Authorization header from the request"""
-        if request is None or 'Authorization' not in request.headers:
-            return None
-        return request.headers['Authorization']
+        """Get the Authorization header from the Flask request.
+        Args:
+        - request: The Flask request object.
+        Returns:
+        - str: The Authorization header or None.
+        """
+        return None
+
 
     def current_user(self, request=None) -> TypeVar('User'):
-        """Get the current user from the request"""
+        """Get the current user from the Flask request.
+        Args:
+        - request: The Flask request object.
+        Returns:
+        - TypeVar('User'): The current user or None.
+        """
         return None
