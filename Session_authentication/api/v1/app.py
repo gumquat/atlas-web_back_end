c58aa6735494baf27be719e7f6651a6a1b8ae268
gumquat <gumquat@gmail.com>
2024-01-22 16:51:13 -0600

    change to 0

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -12,18 +12,17 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
-auth = None
 
 auth_type = os.getenv("AUTH_TYPE")  # Get the AUTH_TYPE environment variable
-if auth_type == 'basic_auth':  # If the AUTH_TYPE is Basic...
-    from api.v1.auth.basic_auth import BasicAuth  # ...import the class...
-    auth = BasicAuth()  # ...then instantiate the class
-elif auth_type == 'session_auth':  # If the AUTH_TYPE is Session...
+if auth_type == 'session_auth':  # If the AUTH_TYPE is Basic...
     from api.v1.auth.session_auth import SessionAuth  # ...import the class...
     auth = SessionAuth()  # ...then instantiate the class
-elif auth_type:  # If the AUTH_TYPE is not empty...
-    from api.v1.auth.auth import Auth  # ...import the class...
-    auth = Auth()  # ...then instantiate the class
+elif auth_type == 'basic_auth':
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
+else:
+    from api.v1.auth.auth import Auth
+    auth = Auth()
 
 
 @app.before_request
@@ -39,14 +38,16 @@ def before_request() -> None:
     # Check if the path is not in the excluded paths list.
     # If the path is not in the excluded paths list,
     # check if authentication is required for the given path.
-    if request.path not in excluded_paths:
-        if auth and auth.require_auth(request.path, excluded_paths):
-            if auth.authorization_header(request) is None:  # If no header
-                abort(401)
-            if auth.current_user(request) is None:  # If no user
-                abort(403)
-            # assign the current user to the request
-            request.current_user = auth.current_user(request)
+    if not auth.require_auth(request.path, excluded_paths):
+        return
+
+    if auth.authorization_header(
+            request) is None and auth.session_cookie(request) is None:
+        abort(401)
+
+    request.current_user = auth.current_user(request)
+    if auth.current_user(request) is None:
+        abort(403)
 
 
 @app.errorhandler(404)
