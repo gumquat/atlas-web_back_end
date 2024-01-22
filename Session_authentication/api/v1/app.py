585af3b51b09a1765f4421a745126e526d8e1130
gumquat <gumquat@gmail.com>
2024-01-22 17:15:27 -0600

    restart from 0

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 """
-Routes for the API
+Route module for the API
 """
-from flask import Flask, jsonify, abort, request
-import os
 from os import getenv
 from api.v1.views import app_views
+from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
+import os
 
 
 app = Flask(__name__)
@@ -14,20 +14,17 @@ app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 
-auth_type = os.getenv("AUTH_TYPE")
-if auth_type == 'session_auth':  # If the AUTH_TYPE is Session...
-    from api.v1.auth.session_auth import SessionAuth  # ...import the class...
-    auth = SessionAuth()  # ...then instantiate the class
-elif auth_type == 'basic_auth':
-    from api.v1.auth.basic_auth import BasicAuth
-    auth = BasicAuth()
-else auth_type == 'auth':
+auth_type = getenv("AUTH_TYPE")
+if getenv('AUTH_TYPE') == 'basic_auth':  # If the AUTH_TYPE is Basic...
+    from api.v1.auth.basic_auth import BasicAuth  # ...import the class...
+    auth = BasicAuth()  # ...then instantiate the class
+else:  # If the AUTH_TYPE is not Basic import and insantiate a different class
     from api.v1.auth.auth import Auth
     auth = Auth()
 
 
 @app.before_request
-def before_request() -> None:
+def before_request():
     """Check if the AUTH_TYPE is Basic, if not, skip this check"""
     if auth is None:
         return
@@ -40,13 +37,11 @@ def before_request() -> None:
     # If the path is not in the excluded paths list,
     # check if authentication is required for the given path.
     if request.path not in excluded_paths:
-        if auth and auth.require_auth(request.path, excluded_paths):
+        if auth.require_auth(request.path, excluded_paths):
             if auth.authorization_header(request) is None:  # If no header
                 abort(401)
             if auth.current_user(request) is None:  # If no user
                 abort(403)
-            # assign the current user to the request
-            request.current_user = auth.current_user(request)
 
 
 @app.errorhandler(404)
--- Session_authentication/api/v1/views/users.py
+++ Session_authentication/api/v1/views/users.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Users views
+""" Module of Users views
 """
 from api.v1.views import app_views
 from flask import abort, jsonify, request
@@ -10,7 +10,7 @@ from models.user import User
 def view_all_users() -> str:
     """ GET /api/v1/users
     Return:
-    - list of all User objects JSON represented
+      - list of all User objects JSON represented
     """
     all_users = [user.to_json() for user in User.all()]
     return jsonify(all_users)
@@ -20,30 +20,27 @@ def view_all_users() -> str:
 def view_one_user(user_id: str = None) -> str:
     """ GET /api/v1/users/:id
     Path parameter:
-    - User ID
+      - User ID
     Return:
-    - User object JSON represented
-    - 404 if the User ID doesn't exist
+      - User object JSON represented
+      - 404 if the User ID doesn't exist
     """
+    if user_id is None:
+        abort(404)
     user = User.get(user_id)
-    if user_id is None or user is None:
+    if user is None:
         abort(404)
-    if user_id == 'me' and request.current_user is None:
-        abort(404)
-    if user_id == 'me' and request.current_user is not None:
-        return jsonify(request.current_user.to_json())
-    else:
-        return jsonify(user.to_json())
+    return jsonify(user.to_json())
 
 
 @app_views.route('/users/<user_id>', methods=['DELETE'], strict_slashes=False)
 def delete_user(user_id: str = None) -> str:
     """ DELETE /api/v1/users/:id
     Path parameter:
-    - User ID
+      - User ID
     Return:
-    - empty JSON is the User has been correctly deleted
-    - 404 if the User ID doesn't exist
+      - empty JSON is the User has been correctly deleted
+      - 404 if the User ID doesn't exist
     """
     if user_id is None:
         abort(404)
@@ -58,13 +55,13 @@ def delete_user(user_id: str = None) -> str:
 def create_user() -> str:
     """ POST /api/v1/users/
     JSON body:
-    - email
-    - password
-    - last_name (optional)
-    - first_name (optional)
+      - email
+      - password
+      - last_name (optional)
+      - first_name (optional)
     Return:
-    - User object JSON represented
-    - 400 if can't create the new User
+      - User object JSON represented
+      - 400 if can't create the new User
     """
     rj = None
     error_msg = None
@@ -96,14 +93,14 @@ def create_user() -> str:
 def update_user(user_id: str = None) -> str:
     """ PUT /api/v1/users/:id
     Path parameter:
-    - User ID
+      - User ID
     JSON body:
-    - last_name (optional)
-    - first_name (optional)
+      - last_name (optional)
+      - first_name (optional)
     Return:
-    - User object JSON represented
-    - 404 if the User ID doesn't exist
-    - 400 if can't update the User
+      - User object JSON represented
+      - 404 if the User ID doesn't exist
+      - 400 if can't update the User
     """
     if user_id is None:
         abort(404)
