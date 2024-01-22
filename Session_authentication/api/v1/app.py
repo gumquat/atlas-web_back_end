7576db596fc05ef80bd109d1350b282728349043
gumquat <gumquat@gmail.com>
2024-01-22 17:48:22 -0600

    changing names

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -13,10 +13,12 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
-if os.getenv('AUTH_TYPE') == 'auth':
+
+auth_type = os.getenv("AUTH_TYPE")
+if auth_type == 'auth':
     from api.v1.auth.auth import Auth
     auth = Auth()
-elif os.getenv('AUTH_TYPE') == 'basic_auth':
+elif auth_type == 'basic_auth':
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
 
