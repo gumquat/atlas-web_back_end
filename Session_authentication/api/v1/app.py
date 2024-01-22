5933b9ed45fad356f9fd4f3de580efe4a101cff8
gumquat <gumquat@gmail.com>
2024-01-22 17:01:45 -0600

    please please please

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -12,17 +12,15 @@ from flask_cors import (CORS, cross_origin)
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
 
-auth_type = os.getenv("AUTH_TYPE")  # Get the AUTH_TYPE environment variable
-if auth_type == 'session_auth':  # If the AUTH_TYPE is Basic...
-    from api.v1.auth.session_auth import SessionAuth  # ...import the class...
-    auth = SessionAuth()  # ...then instantiate the class
-elif auth_type == 'basic_auth':
-    from api.v1.auth.basic_auth import BasicAuth
-    auth = BasicAuth()
-else:
+
+if os.getenv('AUTH_TYPE') == 'auth':
     from api.v1.auth.auth import Auth
     auth = Auth()
+elif os.getenv('AUTH_TYPE') == 'basic_auth':
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
 
 
 @app.before_request
@@ -38,14 +36,15 @@ def before_request() -> None:
     # Check if the path is not in the excluded paths list.
     # If the path is not in the excluded paths list,
     # check if authentication is required for the given path.
-    if request.path not in excluded_paths:
-        if auth and auth.require_auth(request.path, excluded_paths):
-            if auth.authorization_header(request) is None:  # If no header
-                abort(401)
-            if auth.current_user(request) is None:  # If no user
-                abort(403)
-            # assign the current user to the request
-            request.current_user = auth.current_user(request)
+    if not auth.require_auth(request.path, ['/api/v1/status/',
+                                            '/api/v1/unauthorized/',
+                                            '/api/v1/forbidden/']):
+        return
+    if auth.authorization_header(request) is None:
+        abort(401)
+    if auth.current_user(request) is None:
+        abort(403)
+    request.current_user = auth.current_user(request)
 
 
 @app.errorhandler(404)
