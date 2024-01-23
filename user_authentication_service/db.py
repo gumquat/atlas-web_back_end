c7556409c6b17eda09fc8102dd6851c5659ba982
gumquat <gumquat@gmail.com>
2024-01-23 13:38:04 -0600

    revert

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -63,7 +63,7 @@ class DB:
         try:
             user = self._session.query(User).filter_by(**kwargs).first()
             if user is None:
-                raise NoResultFound("No user found with the specified criteria.")
+                raise NoResultFound("No user found.")
             return user
         except InvalidRequestError as e:
             raise InvalidRequestError(f"Invalid request: {e}")
