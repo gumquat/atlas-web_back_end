597b6b226054c52f78f05594e696f0e4f648bb39
gumquat <gumquat@gmail.com>
2024-01-22 18:33:52 -0600

    TYPO

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -10,22 +10,28 @@ class SessionAuth(Auth):
     'Session Auth' class
     """
 
-    user_id_by_seession_id = {}
+    user_id_by_session_id = {}
 
     def create_session(self, user_id: str = None) -> str:
+        """Create a session for the given user ID.
+        Args:
+            user_id (str, optional): The user ID. Defaults to None.
+        Returns:
+            str: The session ID.
         """
-        Creates a Session ID for a user_id.
-        """
-        if user_id is None or type(user_id) is not str:
+        if user_id is None or not isinstance(user_id, str):
             return None
-
-        session_id = str(uuid.uuid4())
+        session_id = str(uuid.uuid4()) # Generate a Session ID
+        # use this sessionID as key for dictionary user_id_by_session_id
         self.user_id_by_session_id[session_id] = user_id
         return session_id
 
     def user_id_for_session_id(self, session_id: str = None) -> str:
-        """
-        Returns a User ID based on a Session ID.
+        """Returns User ID based on a Session's ID
+        Args:
+            session_id (str, optional): The session ID. Defaults to None.
+        Returns:
+            str: The User ID.
         """
         if session_id is None or type(session_id) is not str:
             return None
