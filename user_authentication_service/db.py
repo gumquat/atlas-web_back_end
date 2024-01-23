bf0d57f923a57084c6d3ffbf08f24c103fbc6182
gumquat <gumquat@gmail.com>
2024-01-23 13:40:37 -0600

    rewrite add_user to be better for future dependant code

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
+
 """DB module
 """
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-
+from sqlalchemy.orm.exc import NoResultFound, InvalidRequestError
 from user import Base, User
 
 
@@ -31,9 +33,30 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """Add a new user to the database
+        """adds new user to db
+        Args:
+            email (str): user email
+            hashed_password (str): user pword
+        Returns:
+            User: user as an object
         """
         new_user = User(email=email, hashed_password=hashed_password)
         self._session.add(new_user)
         self._session.commit()
         return new_user
+
+    def find_user_by(self, **kwargs) -> User:
+        """Find a user in the database based on input arguments
+        Args:
+            Self:
+            Kwargs:
+        Returns:
+            User:
+        """
+        try:
+            user = self._session.query(User).filter_by(**kwargs).first()
+            if user is None:
+                raise NoResultFound("No user found.")
+            return user
+        except InvalidRequestError as e:
+            raise InvalidRequestError(f"Invalid request: {e}")
