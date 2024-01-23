8fd0f9ef4b8a993c1a3da24dbd694bf91a1efa97
gumquat <gumquat@gmail.com>
2024-01-22 18:23:11 -0600

    test 1

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -8,4 +8,20 @@ class SessionAuth(Auth):
     """
     'Session Auth' class
     """
-    pass
+
+    user_id_by_seession_id = {}
+    
+    def create_session(self, user_id: str = None) -> str:
+        """Create a session for the given user ID.
+        Args:
+            user_id (str, optional): The user ID. Defaults to None.
+        Returns:
+            str: The session ID.
+        """
+        if user_id is None or not isinstance(user_id, str):
+            return None
+        else:
+            session_id = str(uuid.uuid4()) # Generate a Session ID
+            # Use this sessionID as key for dictionary user_id_by_session_id
+            self.user_id_by_session_id[session_id] = user_id
+            return session_id
