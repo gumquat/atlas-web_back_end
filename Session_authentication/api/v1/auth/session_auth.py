87c325f22b2877d095e9f9a885c68e7660a0b724
gumquat <gumquat@gmail.com>
2024-01-22 18:38:24 -0600

    type cahnge

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -33,7 +33,7 @@ class SessionAuth(Auth):
         Returns:
             str: The User ID.
         """
-        if session_id is None or type(session_id) is not str:
+        if session_id is None or not isinstance(session_id, str):
             return None
         return self.user_id_by_session_id.get(session_id)
 
