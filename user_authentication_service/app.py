1c97ea25217f3d65b92b3c3e0ae3084c9bccc3de
gumquat <gumquat@gmail.com>
2024-01-24 15:44:59 -0600

    test

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -149,7 +149,6 @@ def update_password():
     return jsonify({"email": email, "message": "Password updated"}), 200
 
 
-
 if __name__ == "__main__":
     # Run the app on host 0.0.0.0 and port 5000
     app.run(host="0.0.0.0", port=5000)
