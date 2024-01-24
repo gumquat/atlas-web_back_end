76ffa6340cb36ca72793af0eb5a13deee288011b
gumquat <gumquat@gmail.com>
2024-01-24 11:36:18 -0600

    fuck this stupid shit i hate it

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python3
 """DB module
 """
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import InvalidRequestError
 from sqlalchemy.orm.exc import NoResultFound
 
 from user import Base, User
 
 
 class DB:
-    """DB class
-    """
+    """DB class"""
 
     def __init__(self) -> None:
         """Initialize a new DB instance
@@ -32,7 +32,12 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """Add a new user to the database
+        """adds new user to db
+        Args:
+            email (str): user email
+            hashed_password (str): user pword
+        Returns:
+            User: user as an object
         """
         new_user = User(email=email, hashed_password=hashed_password)
         self._session.add(new_user)
@@ -40,24 +45,17 @@ class DB:
         return new_user
 
     def find_user_by(self, **kwargs) -> User:
-        """Finds a user by arbitrary keyword arguments.
+        """Find a user in the database based on input arguments
+        Args:
+            Self:
+            Kwargs:
+        Returns:
+            User:
         """
         try:
-            query = self._session.query(User).filter_by(**kwargs).first()
-            if query is None:
-                raise NoResultFound
-            return query
-        except InvalidRequestError:
-            raise
-
-    def update_user(self, user_id: int, **kwargs) -> None:
-        """Update a users set attributes
-        """
-        user = self.find_user_by(id=user_id)
-
-        for key, value in kwargs.items():
-            if not hasattr(user, key):
-                raise ValueError
-            setattr(user, key, value)
-
-        self._session.commit()
+            user = self._session.query(User).filter_by(**kwargs).first()
+            if user is None:
+                raise NoResultFound("No user found.")
+            return user
+        except InvalidRequestError as e:
+            raise InvalidRequestError(f"Invalid request: {e}")
