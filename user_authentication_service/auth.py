89233e81ba229217a088900221ff825aa2122705
gumquat <gumquat@gmail.com>
2024-01-24 11:45:48 -0600

    _hash_password (4)

---
+++ user_authentication_service/auth.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+""" 'Auth' model """
+import bcrypt
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
+import uuid
+
+
+class Auth:
+    """allows for user authentication
+        and working with the database of users
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def _hash_password(self, password: str) -> bytes:
+        """hashes passwords
+        """
+        return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
