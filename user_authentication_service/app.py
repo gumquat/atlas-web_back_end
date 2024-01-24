6b1ba501c1859a56f0da24adc966cf72d4e95927
gumquat <gumquat@gmail.com>
2024-01-24 12:28:27 -0600

    pycodestyle

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -7,11 +7,13 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def payload():
     """returns json string, aka a payload
     """
     return jsonify({"message": "Bienvenue"})
+
 
 if __name__ == "__main__":
     # Run the app on host 0.0.0.0 and port 5000
