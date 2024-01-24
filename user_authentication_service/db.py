655507182aafccfc148ecb80a7479ece7fae4032
gumquat <gumquat@gmail.com>
2024-01-24 11:37:13 -0600

    WHY

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -1,21 +1,21 @@
 #!/usr/bin/env python3
 """DB module
 """
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-from sqlalchemy.orm.exc import InvalidRequestError
+from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.orm.exc import NoResultFound
 
 from user import Base, User
 
 
 class DB:
-    """DB class"""
+    """DB class
+    """
 
     def __init__(self) -> None:
-        """Initialize a new DB instance
+        """Init DB instance
         """
         self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
@@ -32,30 +32,9 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """adds new user to db
-        Args:
-            email (str): user email
-            hashed_password (str): user pword
-        Returns:
-            User: user as an object
+        """Add a new user to db
         """
         new_user = User(email=email, hashed_password=hashed_password)
         self._session.add(new_user)
         self._session.commit()
         return new_user
-
-    def find_user_by(self, **kwargs) -> User:
-        """Find a user in the database based on input arguments
-        Args:
-            Self:
-            Kwargs:
-        Returns:
-            User:
-        """
-        try:
-            user = self._session.query(User).filter_by(**kwargs).first()
-            if user is None:
-                raise NoResultFound("No user found.")
-            return user
-        except InvalidRequestError as e:
-            raise InvalidRequestError(f"Invalid request: {e}")
