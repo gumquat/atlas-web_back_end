047e39b0ea70a27b02b242eb26aed6fbb43d5d54
gumquat <gumquat@gmail.com>
2024-01-24 15:13:18 -0600

    logout (14)

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -81,6 +81,16 @@ def login():
         abort(401, "Incorrect login information")
 
 
+@app.route('/sessions', methods=['DELETE'], strict_slashes=False)
+def logout():
+    """i wonder what this does"""
+    session_id = request.cookies.get('session_id', None)
+    user = AUTH.get_user_from_session_id(session_id)
+    if session_id is None or user is None:
+        abort(403)
+    AUTH.destroy_session(user.id)
+    return redirect('/')
+
 if __name__ == "__main__":
     # Run the app on host 0.0.0.0 and port 5000
     app.run(host="0.0.0.0", port=5000)
