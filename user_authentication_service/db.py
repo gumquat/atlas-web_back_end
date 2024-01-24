7e87e7bdc3fce3d617755581fde7289439f4bd8b
gumquat <gumquat@gmail.com>
2024-01-24 11:34:51 -0600

    test

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python3
 """DB module
 """
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-from sqlalchemy.orm.exc import InvalidRequestError
+from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.orm.exc import NoResultFound
 
 from user import Base, User
 
 
 class DB:
-    """DB class"""
+    """DB class
+    """
 
     def __init__(self) -> None:
         """Initialize a new DB instance
@@ -32,12 +32,7 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """adds new user to db
-        Args:
-            email (str): user email
-            hashed_password (str): user pword
-        Returns:
-            User: user as an object
+        """Add a new user to the database
         """
         new_user = User(email=email, hashed_password=hashed_password)
         self._session.add(new_user)
@@ -45,17 +40,24 @@ class DB:
         return new_user
 
     def find_user_by(self, **kwargs) -> User:
-        """Find a user in the database based on input arguments
-        Args:
-            Self:
-            Kwargs:
-        Returns:
-            User:
+        """Finds a user by arbitrary keyword arguments.
         """
         try:
-            user = self._session.query(User).filter_by(**kwargs).first()
-            if user is None:
-                raise NoResultFound("No user found.")
-            return user
-        except InvalidRequestError as e:
-            raise InvalidRequestError(f"Invalid request: {e}")
+            query = self._session.query(User).filter_by(**kwargs).first()
+            if query is None:
+                raise NoResultFound
+            return query
+        except InvalidRequestError:
+            raise
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """Update a users set attributes
+        """
+        user = self.find_user_by(id=user_id)
+
+        for key, value in kwargs.items():
+            if not hasattr(user, key):
+                raise ValueError
+            setattr(user, key, value)
+
+        self._session.commit()
