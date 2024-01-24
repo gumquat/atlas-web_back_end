72d2f84803d9dfb9a2b60f309b049c949bec2cbf
gumquat <gumquat@gmail.com>
2024-01-24 14:56:33 -0600

    pycodestyle

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -80,6 +80,7 @@ def login():
     except ValueError:
         abort(401, "Incorrect login information")
 
+
 if __name__ == "__main__":
     # Run the app on host 0.0.0.0 and port 5000
     app.run(host="0.0.0.0", port=5000)
