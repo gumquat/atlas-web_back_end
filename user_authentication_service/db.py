d53d71fa1a6ddef315caf8f857546cbb592101d1
gumquat <gumquat@gmail.com>
2024-01-24 11:41:43 -0600

    okay so the last one was luck im actuall shit tho

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -49,3 +49,13 @@ class DB:
             return query
         except InvalidRequestError:
             raise
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """Update a user's attriubutres
+        """
+        user = self.find_user_by(id=user_id)
+        for key, value in kwargs.items():
+            if not hasattr(user, key):
+                raise ValueError
+            setattr(user, key, value)
+        self._session.commit()
