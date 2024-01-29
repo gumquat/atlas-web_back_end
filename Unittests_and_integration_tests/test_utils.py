cf374bb0a07f3b6c49286e4e0837e2055ff6b809
gumquat <gumquat@gmail.com>
2024-01-29 10:54:24 -0600

    testing testing 123

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -30,3 +30,46 @@ class TestAccessNestedMap(unittest.TestCase):
         with self.assertRaises(KeyError) as cm:
             access_nested_map(nested_map, path)
         self.assertEqual(cm.exception.args[0], path[-1])
+
+
+class TestGetJson(unittest.TestCase):
+    """CLASS - test the `get_json` function 'utils.py'
+    """
+    # below are inputs to be tested in 'test_get_json'
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    def test_get_json(self, url, payload):
+        """tests that `utils.get_json` returns correctly
+        """
+        mock_response = Mock()
+        mock_response.json.return_value = payload
+        with patch('requests.get', return_value=mock_response) as mock_get:
+            response = get_json(url)
+            mock_get.assert_called_once_with(url)
+            self.assertEqual(response, payload)
+
+
+class TestMemoize(unittest.TestCase):
+    """CLASS - test the `memoize` function from 'utils.py'
+    """
+
+    def test_memoize(self):
+        """test `memoize` with a TestClass class
+        """
+        class TestClass:
+
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        with patch.object(TestClass, 'a_method',
+                          return_value=42) as mock_method:
+            test_class = TestClass()
+            self.assertEqual(test_class.a_property, 42)
+            self.assertEqual(test_class.a_property, 42)
+            mock_method.assert_called_once()
