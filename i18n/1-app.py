a56b0a3cf43827aa2689fed219dbba698e279cea
gumquat <gumquat@gmail.com>
2024-01-30 11:36:31 -0600

    Basic Babel Setup

---
+++ i18n/1-app.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""basic flask app with index.html template
+"""
+from flask import Flask, render_template
+from flask_babel import Babel
+
+app = Flask(__name__)
+
+
+class Config():
+    """CLASS - configures available languages
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)  # set Babel's default locale and timezone
+babel = Babel(app)  # initialize Babel in the app
+
+
+@app.route('/')
+def index():
+    """`/` route
+    """
+    return render_template('1-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
