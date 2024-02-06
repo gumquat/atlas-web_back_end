c05a81e99a456d14428e71d3ab09c2baf9398abc
gumquat <gumquat@gmail.com>
2024-02-06 11:37:41 -0600

    revert

--- MySQL_Advanced/1-country_users.sql
+++ MySQL_Advanced/1-country_users.sql
@@ -1,6 +1,6 @@
 -- SQL script that creates a table `users`
 CREATE TABLE IF NOT EXISTS users (
-    id SERIAL PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255)
     country ENUM('US', 'CO', 'TN') NOT NULL
