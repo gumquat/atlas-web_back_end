8be1ca8003654922560101963add12f0077e2e9e
gumquat <gumquat@gmail.com>
2024-02-06 12:18:48 -0600

    this should work

--- MySQL_Advanced/2-fans.sql
+++ MySQL_Advanced/2-fans.sql
@@ -1,6 +1,6 @@
 -- ranks country origins of bands, ordered
 -- by the number of (non-unique) fans
-SELECT origin, SUM(fans) AS nb_fans
+SELECT origin, SUM(fans) AS _fans
 FROM metal_bands
 GROUP BY origin
-ORDER BY nb_fans DESC;
+ORDER BY _fans DESC;
