28c27ecf24178b9e3838e783ff41d5a774fa3e14
gumquat <gumquat@gmail.com>
2024-02-07 12:44:18 -0600

    begin #6

---
+++ 0x0B_redis_basic/6-bonus.sql
@@ -0,0 +1,9 @@
+-- create a stored procedure `AddBonus` that
+-- adds a new correction for a student
+
+DELIMITER //
+
+CREATE PROCEDURE AddBonus (IN user_id INT, IN project_name VARCHAR (255), IN score INT)
+BEGIN
+    
+END;
