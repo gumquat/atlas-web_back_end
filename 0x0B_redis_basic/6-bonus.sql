27647d7c12ccf2c5f388875ecc4d8f3abe02fbef
gumquat <gumquat@gmail.com>
2024-02-07 12:52:58 -0600

    Add Bonus #6

--- 0x0B_redis_basic/6-bonus.sql
+++ 0x0B_redis_basic/6-bonus.sql
@@ -5,5 +5,16 @@ DELIMITER //
 
 CREATE PROCEDURE AddBonus (IN user_id INT, IN project_name VARCHAR (255), IN score INT)
 BEGIN
-    
-END;
+    -- hold the project ID in an INT variable
+    DECLARE project_id INT;
+    -- select the project ID of the given project name
+    SELECT id INTO project_id FROM projects WHERE name = project_name;
+    -- check if the project exists
+    IF project_id IS NULL THEN
+        -- if it doesnt exist, insert a new project with the given name
+        INSERT INTO projects (name) VALUES (project_name);
+        -- get the last inserted project's ID
+        SET project_id = LAST_INSERT_ID();
+END; //
+
+DELIMITER;
