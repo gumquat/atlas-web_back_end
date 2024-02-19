3c01646e33c6fb5c826c5f7cb8352912b93a29c8
gumquat <gumquat@gmail.com>
2024-02-19 17:01:32 -0600

    lint change

--- Node_JS_basic/0-console.js
+++ Node_JS_basic/0-console.js
@@ -5,5 +5,6 @@ function displayMessage(message) {
   // output the given data as a message to the console
   console.log(message);
 }
+
 //allows other files to use the function 'displayMessage'
 module.exports = displayMessage;
