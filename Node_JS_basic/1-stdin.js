be3cb2a430a843d2a2b83f5f891c8695fdae2490
gumquat <gumquat@gmail.com>
2024-02-19 16:10:33 -0600

    change to 1

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -6,7 +6,7 @@
 console.log('Welcome to Holberton School, what is your name?');
 
 process.stdin.on('data', data => {
-    console.log(`Your name is: ${data.toString()}`);
-    console.log('This important software is now closing')
-    process.exit();
-    });
+  console.log(`Your name is: ${data.toString()}`);
+  console.log('This important software is now closing')
+  process.exit();
+  });
