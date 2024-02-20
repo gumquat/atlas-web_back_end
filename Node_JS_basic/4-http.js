1d6de3931409c5703b0132554cb29a1b4fe65f34
gumquat <gumquat@gmail.com>
2024-02-20 11:25:03 -0600

    whoops make it listen to the port in the createServer func

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -4,10 +4,10 @@ const app = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' });
   res.end('Hello Holberton School!\n');
   res.end();
-});
+}).listen(1245);
 
-app.listen(1245, () => {
-  console.log('Server is running on port 1245');
-});
+// app.listen(1245, () => {
+//   console.log('Server is running on port 1245');
+// });
 
 module.exports = app;
