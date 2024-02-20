384cba4178812655b036a616e2278276584e974d
gumquat <gumquat@gmail.com>
2024-02-20 11:36:09 -0600

    revert 0-console

--- Node_JS_basic/0-console.js
+++ Node_JS_basic/0-console.js
@@ -2,7 +2,6 @@
 // function named displayMessage that prints in STDOUT the string argument
 
 function displayMessage(message) {
-  // output the given data as a message to the console
   console.log(message);
 }
 
