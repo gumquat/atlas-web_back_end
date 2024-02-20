175e232238ec122a5ce830535582b61e4f8fa3fe
gumquat <gumquat@gmail.com>
2024-02-20 13:41:10 -0600

    ESLINTERRRRR

--- Node_JS_basic/6-http_express.js
+++ Node_JS_basic/6-http_express.js
@@ -4,11 +4,11 @@ const app = express();
 const port = 1245;
 
 app.get('/', (req, res) => {
-    res.send('Hello Holberton School!');
+  res.send('Hello Holberton School!');
 });
 
 app.listen(port, () => {
-    console.log(`Listening on port ${port}.`);
+  console.log(`Listening on port ${port}.`);
 });
 
 module.exports = app;
