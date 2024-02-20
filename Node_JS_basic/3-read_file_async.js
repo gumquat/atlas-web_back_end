a37b8351bf08f16d4b94d6d4d4a75a9a0fd71268
gumquat <gumquat@gmail.com>
2024-02-20 15:12:30 -0600

    ESlint padding

--- Node_JS_basic/3-read_file_async.js
+++ Node_JS_basic/3-read_file_async.js
@@ -2,25 +2,19 @@ const fs = require('fs').promises;
 
 async function countStudents(path) {
   try {
-
     // await for async purposes
     const data = await fs.readFile(path, 'utf8');
-
     // split the data into rows
     // remove empty lines
     const rows = data.split('\n').filter(row => row.trim());
-
     // extract headers from the header row
     const [headerRow, ...studentRows] = rows;
     const headers = headerRow.split(',');
-
     // find index of 'field' and 'firstname' in header
     const fieldIndex = headers.indexOf('field');
     const firstNameIndex = headers.indexOf('firstname');
-
     // get count of total number of students
     console.log(`Number of students: ${studentRows.length}`);
-
     // group students by field
     // count them
     const studentsByField = studentRows.reduce((acc, row) => {
