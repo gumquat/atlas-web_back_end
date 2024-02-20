24e71fe84ad7481e2a2d94274f20e75b1c02d1d4
gumquat <gumquat@gmail.com>
2024-02-20 15:02:20 -0600

    test

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,3 +1,7 @@
+// 1-stdin.js
+// program that will be executed through command line, displaying messages
+// and displays user input
+
 // Program that displays a string, takes inputs, and responds
 
 console.log('Welcome to Holberton School, what is your name?');
@@ -12,3 +16,22 @@ process.stdin.on('readable', () => {
 process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
 });
+
+/**the code below fails ONE check but OTHERWISE WORKS FINE
+// display welcome message
+console.log('Welcome to Holberton School, what is your name?');
+
+// read user input from standard input 'stdin'
+process.stdin.on('data', (data) => {
+  // remove trailing newline character from input
+  const name = data.toString().trim();
+
+  // display the user's input
+  console.log(`Your name is: ${name}`);
+});
+
+// display closing message when user chooses to 'end' it
+process.stdin.on('end', () => {
+  console.log('This important software is now closing');
+});
+*/
