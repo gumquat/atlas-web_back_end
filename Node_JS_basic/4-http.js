87ec91369a4cc2a0882d8a8e37d59f5f007dc2d8
gumquat <gumquat@gmail.com>
2024-02-20 11:24:16 -0600

    added 'end()'

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -3,6 +3,7 @@ const http = require('http');
 const app = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' });
   res.end('Hello Holberton School!\n');
+  res.end();
 });
 
 app.listen(1245, () => {
